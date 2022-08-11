fbec88236873749cdc9fa844ac2e332a56194608
Jik789 <stix.jik@gmail.com>
2022-08-11 10:14:49 +0300

    feat: add presenter

--- src/main.js
+++ src/main.js
@@ -2,22 +2,14 @@
 
 import FilterView from './view/filter-view';
 import SortView from './view/sort-view';
-import EventListView from './view/event-list';
-import FormAddView from './view/form-add-view';
-import FormEditView from './view/form-edit-view';
-import WaypointView from './view/waypoint-view';
 import { render } from './render.js';
+import EventListPresenter from './presenter/event-list-presenter';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteTripEventElement = document.querySelector('.trip-events');
+const eventListPresenter = new EventListPresenter();
 
 render(new FilterView(), siteFilterElement);
 render(new SortView(), siteTripEventElement);
-render(new EventListView(), siteTripEventElement);
 
-const siteEventListElement = document.querySelector('.trip-events__list');
-render(new FormEditView(), siteEventListElement);
-render(new FormAddView(), siteEventListElement);
-render(new WaypointView(), siteEventListElement);
-render(new WaypointView(), siteEventListElement);
-render(new WaypointView(), siteEventListElement);
+eventListPresenter.init(siteTripEventElement);
