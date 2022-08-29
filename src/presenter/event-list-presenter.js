6892a66823f465692af65c3d6a55203ab3e0ffa2
Jik789 <stix.jik@gmail.com>
2022-08-29 11:44:22 +0300

    fix: sl presenter

--- src/presenter/event-list-presenter.js
+++ src/presenter/event-list-presenter.js
@@ -8,16 +8,17 @@ import WaypointPresenter from './waypoint-presenter';
 
 export default class EventListPresenter {
   #eventListComponent = new EventListView();
+  #noWaypointComponent = new NoWaypointView();
 
   init = (parentContainer) => {
     this.parentContainer = parentContainer;
     this.waypointsModel = new WaypointModel();
     this.waypoints = this.waypointsModel.waypoints;
 
-    render(this.#eventListComponent, this.parentContainer);
+    this.#renderEventList();
 
     if (!(this.waypoints.length > 0)) {
-      render(new NoWaypointView(), this.#eventListComponent.element);
+      this.#renderNoWayPoints();
     } else {
       for (let i = 0; i < this.waypoints.length; i++) {
         this.#renderWayPoints(this.waypoints[i]);
@@ -28,5 +29,13 @@ export default class EventListPresenter {
   #renderWayPoints = (wayPoint) => {
     const waypointPresenter = new WaypointPresenter(this.#eventListComponent.element);
     waypointPresenter.init(wayPoint);
+  };
+
+  #renderNoWayPoints = () => {
+    render(this.#noWaypointComponent, this.#eventListComponent.element);
+  };
+
+  #renderEventList = () => {
+    render(this.#eventListComponent, this.parentContainer);
   };
 }
