344c74a0142b3e693e17378f00fab268cd4fb72e
Jik789 <stix.jik@gmail.com>
2022-09-02 11:15:47 +0300

    feat: чередной фикс багов, с формой редактирования карточки, снова

--- src/mock/waypoint-mock.js
+++ src/mock/waypoint-mock.js
@@ -7,6 +7,6 @@ export const waypointMock = (keyId) => ({
   'dateTo': getRandomValue(DATE_TO_MOCK),
   'destination': keyId,
   'id': keyId,
-  'offers': Array.from(new Set([getRandomNumberOfRange(1, 4), getRandomNumberOfRange(1, 4), getRandomNumberOfRange(1, 4), getRandomNumberOfRange(1, 4)])), //Во тут будет приходить статичное значение, по этому не должно быть проблем
+  'offers': Array.from(new Set([getRandomNumberOfRange(1, 4), getRandomNumberOfRange(1, 4), getRandomNumberOfRange(1, 4), getRandomNumberOfRange(1, 4)])),
   'type': getRandomValue(WAYPOINT_TYPE_MOCK),
 });
