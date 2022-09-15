75fdfa3a63e59176e428938084a52496fe9cd68e
Jik789 <stix.jik@gmail.com>
2022-09-15 12:49:22 +0300

    Модуль 6, задание 2 начало

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import { render } from './render.js';
 import FilterView from './view/filter-view';
 import EventListPresenter from './presenter/board-presenter';
-import WaypointModel from './model/waypoint-model.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteTripEventElement = document.querySelector('.trip-events');
